2c8bfd20ba371aaa89d22510ea0509121725e1fa
Giovanni Carpentier <giovannicarpentier@outlook.com>
2023-05-17 18:06:46 +0200

    basic outline of the script

---
+++ main.js
@@ -0,0 +1,21 @@
+/* 
+
+Main flow of the progam
+
+# Content Scraping
+    Scrape content from the web (ex. Reddit) to post on the theme page
+    Save the scraped content to a file that the script can use to post that content
+    Post the saved content to Instagram X times a day
+
+# Growth Hacking
+    Common way to gain followers is the follow - unfollow technique 
+    The script needs to read a list of account simular to our themepage 
+    Grab a list of people to follow based on the people that liked a simular page's posts
+    Follow X amount of people per hour and try to stay under Instagram's limits
+    Save the followed people to a new list and delete them from the to follow list
+    start unfollowing people after x amount of time
+
+    Considerations
+        Don't follow people that already follow our page
+
+*/
